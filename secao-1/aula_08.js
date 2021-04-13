1c4027ddc1e1be6f1d0e978f5843008cdcb6d49a
jhameson <jhamesonlucas22@gmail.com>
2021-04-13 14:47:53 -0300

    Console.log()

---
+++ secao-1/aula_08.js
@@ -0,0 +1,13 @@
+//console.log()  => exibe uma informação no console
+console.log("Jhameson");
+console.log('Jhameson');
+console.log(`Jhameson`);
+
+var nome = 'Jhameson';
+console.log('Nome: '+nome)
+console.log(`Nome: ${nome}`);
+
+var n1 = 5, n2 = 8, n3 = 3.2;
+console.log(`Soma ${n1+n2}`);
+console.log(`Multiplicacao ${n1*n3}`);
+
